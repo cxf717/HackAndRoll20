a3972d7551ca22dac22ef695b43df53938d52b7d
Sarah <sarahlim2612@gmail.com>
2020-01-18 20:57:24 +0800

    Create database upon adding bot
    Able to set up database when bot is added and add in a user when they click join game. Buggy output.

--- DurianKing.py
+++ DurianKing.py
@@ -6,6 +6,7 @@ import json
 import os
 import requests
 import logging
+from dbhelper import DBHelper
 
 
 ###### Bot Setup #######
@@ -23,10 +24,8 @@ with open('token.ini', 'r') as file:
 # Create the bot
 updater = Updater(token=BOT_TOKEN, use_context=True)
 
-
-
-
-
+# Setup database when bot is started
+db = DBHelper()
 
 ######## Main Code ###########
 
@@ -39,7 +38,12 @@ def start(update, context):
             chat_id=update.effective_chat.id,
             text=f'Please start the game in a group!'
         )
-    elif (chat_type == "group" or chat_type == "supergroup"):     
+    elif (chat_type == "group" or chat_type == "supergroup"):  
+        print("start db setup")
+        db.delete_all_users()
+        db.setup()
+        print("end db setup")
+
         context.bot.send_message(
             chat_id=update.effective_chat.id,
             text=f'You have started a game of Durian King. Eee, something stinks!'
@@ -54,19 +58,34 @@ def start(update, context):
             reply_markup=reply_markup
         )
 
-        
-
-
-
+#join game code 
+def join(update, context):
+    query = update.callback_query
+    if query.data == '1':
+        db.add_user(query.from_user.id, query.from_user.username, None, 0)
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f'Yay you have successfully joined the game!'
+        )
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f'{db.get_users}'
+        )
+    else: 
+        print("error with join button")
 
 
 
 ######### Handlers ###########
-#add a handler for /start
+#handler for /start
 updater.dispatcher.add_handler(
     CommandHandler('start', start)
 )
 
+#handler for join game button
+updater.dispatcher.add_handler(
+    CallbackQueryHandler(join)
+)
 
 
 
@@ -79,5 +98,6 @@ print('Bot started!')
 
 # Wait for the bot to stop
 updater.idle()
-
+db.delete_all_users()
+print('Deleted db')
 print('Bot stopped!')
