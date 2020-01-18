da53d6973c2c66f4dd9291284930b265980134ab
Sarah <sarahlim2612@gmail.com>
2020-01-18 23:49:33 +0800

    Allow game to be played in different grps
    Maintain unique table with table name containing unique chat_id for each group chat to enable games to be played separately across different groups simultaneously.

--- DurianKing.py
+++ DurianKing.py
@@ -26,9 +26,6 @@ updater = Updater(token=BOT_TOKEN, use_context=True)
 
 # Setup database when bot is started
 db = DBHelper()
-print("start db setup")
-db.setup()
-print("end db setup")
 
 ######## Main Code ###########
 
@@ -36,18 +33,23 @@ print("end db setup")
 # Only allows the game to start if it is /start in a group
 def start(update, context):
     chat_type = update.effective_message.chat.type
+    chat_id = update.effective_message.chat.id
+
     if (chat_type == "private" or chat_type == "channel"):
         context.bot.send_message(
-            chat_id=update.effective_chat.id,
+            chat_id=chat_id,
             text=f'Please start the game in a group!'
         )
     elif (chat_type == "group" or chat_type == "supergroup"):  
         # clear database
-        db.delete_all_users()
+        print("start db setup")
+        db.setup(chat_id)
+        print("end db setup")
+        db.delete_all_users(chat_id)
         print("clear database")
 
         context.bot.send_message(
-            chat_id=update.effective_chat.id,
+            chat_id=chat_id,
             text=f'You have started a game of Durian King. Eee, something stinks!'
         )
 
@@ -55,7 +57,7 @@ def start(update, context):
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         context.bot.send_photo(
-            chat_id=update.effective_chat.id,
+            chat_id=chat_id,
             photo="https://stickershop.line-scdn.net/stickershop/v1/product/1431176/LINEStorePC/main.png;compress=true",
             reply_markup=reply_markup
         )
@@ -63,31 +65,33 @@ def start(update, context):
 #join game code 
 def join(update, context):
     query = update.callback_query
+    chat_id = update.effective_message.chat.id
+
     if query.data == '1':
-        if db.check_user(query.from_user.id) == 0:
-            db.add_user(query.from_user.id, query.from_user.first_name, None, 0)
+        if db.check_user(query.from_user.id, chat_id) == 0:
+            db.add_user(query.from_user.id, query.from_user.first_name, None, 0, chat_id)
             print("user successfully added")
             context.bot.send_message(
-                chat_id=update.effective_chat.id,
+                chat_id=chat_id,
                 text=f'Yay {query.from_user.first_name} has successfully joined the game!'
             ) 
         else:
             context.bot.send_message(
-                chat_id=update.effective_chat.id,
+                chat_id=chat_id,
                 text=f'{query.from_user.first_name} is already in the game!'
             )
 
-        usernames_list = db.get_usernames()
+        usernames_list = db.get_usernames(chat_id)
 
         context.bot.send_message(
-            chat_id=update.effective_chat.id,
+            chat_id=chat_id,
             text=f'{usernames_list}'
         )
     else: 
         print("error with join button")
     
-    db.get_users()
-    db.get_usernames()
+    db.get_users(chat_id)
+    db.get_usernames(chat_id)
 
 
 ######### Handlers ###########
@@ -112,6 +116,4 @@ print('Bot started!')
 
 # Wait for the bot to stop
 updater.idle()
-db.delete_all_users()
-print('Deleted db')
 print('Bot stopped!')
