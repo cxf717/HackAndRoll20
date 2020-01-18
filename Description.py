26abfadb21c3904b75a70cd711c808d274fac99b
Heng Fu Yuen <heng.fu.yuen@gmail.com>
2020-01-18 17:51:40 +0800

    Description
    Brief outline of message used in game

---
+++ Description.py
@@ -0,0 +1,76 @@
+# MODERATOR
+"Welcome to Singapore! Board at Changi Airport MRT Station to start!"
+"You have boarded the MRT at Changi Airport Station"
+"Use /  to find the meaning for all bolded words."
+"Learn all about 
+
+# CHAT
+" xxx has started, board at Changi Airport MRT station to start!"
+
+"Welcome to Singapore"
+"PEP, PEP , PEP Thank you for The MRT is moving off"
+"Next station, xxx"
+"Please do not bring durians on board trains"
+"Please do not eat or drink on board trains."
+"Attention please, if you see anyone suspicious , please inform xxx"
+"Next station, xxx. This train service will end at xxx" # force stop the game
+"<station>. Please mind the platform gap.(if underground)"
+"I can't take the durian smell anymore! xxx flees from the MRT"
+
+#TUTORIAL & DESCRIPTION OF TERMS
+"MRT = Mass Rapid Transit"
+"One of the main modes of transport in the little red dot"
+
+#DESCRIPTION OF ROLES
+### GOOD WITH SPECIAL ROLES ###
+
+    #Influencer
+"Influencer"
+"ROLE IG: Influences someone's vote next round"
+
+
+    #Pretend sleeper
+"Pretend Sleeper"
+"Pretends to sleep so that they do not have to give up their seats"
+"ROLE IG: Obstruct someone from voting next round"
+
+    #Stomper
+"Stomper"
+"Your local version of citizen reporters here in Singapore that captures your everymove and posts them online"
+"ROLE IG: equipped with infrared camera to help catch the lawless while MRT passesthrough the tunnel"
+
+###USELESS GOOD###
+    #Aunty
+"Aunty"
+
+    #Uncle
+"Uncle"
+
+    #Ah Boy
+"Ah Boy"
+
+    #Ah Girl
+"Ah Girl"
+
+###THE LAW BREAKERS/THE FEERLESS###
+    #Durian King
+"Durian King"
+"DURIAN IS BAN ON THE TRAIN"
+
+    #Durian Queen
+"Durian Queen"
+
+
+    #The snacker
+"The Snacker"
+
+    #Drunk on juice
+"Drunk on juice"
+
+
+#FUNCTIONS
+    #/help
+    #/start
+    #/Player Description
+    #/terms
+
