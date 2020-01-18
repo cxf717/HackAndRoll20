d8f5759e51e2f4d388acd9f3f2d19a61b1c0e43e
Heng Fu Yuen <heng.fu.yuen@gmail.com>
2020-01-19 06:30:50 +0800

    Classes for roles

---
+++ PassengerClass.py
@@ -0,0 +1,20 @@
+class passenger(object):
+    def __init__(self, role, description):
+        self.role = role
+        self.description = description
+
+    def role(self):
+        print(self.role)
+        # return self.role
+
+    def description(self):
+        print(self.description)
+        # return self.description
+
+class DurianLover(passenger):
+    def __init__(self):
+            super().__init__("Durian Lover", "You are the Durian Lover!" + "\U0001f60d")
+            
+
+	
+
